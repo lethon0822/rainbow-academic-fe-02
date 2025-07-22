9277238dca3f9996f35969844d4478626d56e70c
ongsoony8382 <ongsoony8382@gmail.com>
2025-07-22 15:46:50 +0900

    수강 신청 구현 및 수강 신청 실패 분기처리 작업 중

--- src/views/Enrollment.vue
+++ src/views/Enrollment.vue
@@ -5,6 +5,7 @@ import CourseTable from '@/components/CourseTable.vue';
 import { getDepartments, getYears } from '@/services/CourseService';
 import { ref, onMounted } from 'vue';
 import { getCourseListByFilter } from '@/services/CourseService';
+import { postEnrollCourse } from '@/services/SugangService';
 
 const departments = ref([]);
 const years = ref([]);
@@ -17,24 +18,67 @@ onMounted(async () => {
   const yearRes = await getYears();
   console.log(yearRes.data);
   years.value = yearRes.data;
-})
+});
 
-const handleSearch = async(filters) => {
+const handleSearch = async (filters) => {
   console.log(' 받은 필터:', filters);
 
   const courseListRes = await getCourseListByFilter(filters);
   console.log('courseListRes: ', courseListRes);
   courseList.value = courseListRes.data;
+};
 
-}
+const handleEnroll = async (course) => {
+  const sugangReq = {
+    courseId: course.courseId,
+  };
+
+  console.log('수강신청할 강의: ', sugangReq);
+
+  try{
+    const sugangRes = await postEnrollCourse(sugangReq);
+    console.log('sugangRes: ', sugangRes);
+    sugang.value = sugangRes.data;
+
+    if(sugangRes.status === 200){
+      alert('수강신청 완료!');
+    }
+
+  } catch(error){
+      const err = error.response.data;
+      console.log(err);
+      switch (err.code){
+        case 4002: 
+          alert('정원 초과! 수강신청에 실패하였습니다.');
+          break;
+
+        case 5001: 
+          alert('서버 오류! 수강신청에 실패하였습니다.');
+          break;
+        
+        default:
+          alert('알 수 없는 오류 발생! 수강신청에 실패하였습니다.');
+      }
+
+  }
+
+
+
+
+};
 
 const courseList = ref([]);
-
+const sugang = ref({});
 </script>
 
 <template>
   <WhiteBox title="수강 신청">
-    <SearchFilterBar :state="true" :departments="departments" :years="[{ year: 2025 }]"   @search="handleSearch"></SearchFilterBar>
+    <SearchFilterBar
+      :state="true"
+      :departments="departments"
+      :years="[{ year: 2025 }]"
+      @search="handleSearch"
+    ></SearchFilterBar>
 
     <h5 class="fw-bold mt-3">개설 과목 목록</h5>
     <CourseTable
