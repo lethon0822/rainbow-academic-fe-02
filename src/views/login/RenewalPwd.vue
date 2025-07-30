65e4f7ea467d6ae35bb881120a0271d08bab9794
1ethon0822 <1ethyjj9240@gmail.com>
2025-07-30 17:22:31 +0900

    새 비밀번호 입력 모달창 띄우기

--- src/views/login/RenewalPwd.vue
+++ src/views/login/RenewalPwd.vue
@@ -1,25 +1,29 @@
 <script setup>
 import { reactive } from 'vue';
-import { findId } from '@/services/accountService';
+import { useRouter } from 'vue-router';
 
+const router = useRouter();
 const state = reactive({
   data: {
     email: '',
-    auth: ''
+    auth: '',
   },
 });
 
 const submit = async () => {
-  const res = await findId(state.form);
-  state.data = res.data;
-
+  router.push('/new');
+  isModalOpen.value = true;
 };
 </script>
 
 <template>
   <h2 class="title">비밀번호 변경</h2>
   <div class="container">
-    <form class="py-5 d-flex flex-column gap-3" @submit.prevent="submit">
+    <form
+      class="py-4 d-flex flex-column gap-3"
+      @submit.prevent="submit"
+      v-if="!state.data.email"
+    >
       <label class="labels">이메일:</label>
       <div class="d-flex">
         <input
@@ -28,8 +32,9 @@ const submit = async () => {
           v-model="state.data.email"
           placeholder="이메일을 입력해주세요."
           required
-          인증번호 요청
-          />
+          인증번호
+          요청
+        />
       </div>
       <label class="labels">인증번호:</label>
       <div class="d-flex">
@@ -38,11 +43,38 @@ const submit = async () => {
           class="form-control"
           v-model="state.data.auth"
           placeholder="인증번호를 입력해주세요."
-          />
+        />
         <button class="h6 btn py-3 mt-3 btn-primary auth">인증번호 요청</button>
       </div>
       <button class="w-100 h6 btn py-3 mt-3 btn-primary">제출</button>
     </form>
+
+    <!-- 결과 있을 때만 결과 표시 -->
+    <div class="showId mt-4" v-if="state.data.email">
+      <label class="labels">새 비밀번호:</label>
+      <div class="d-flex">
+        <input
+          type="text"
+          class="form-control"
+          v-model="state.data.newPwd"
+          required
+          인증번호
+          요청
+        />
+      </div>
+      <label class="labels">새 비밀번호 확인:</label>
+      <div class="d-flex">
+        <input
+          type="password"
+          class="form-control"
+          v-model="state.data.confirm"
+          required
+          인증번호
+          요청
+        />
+      </div>
+      <button class="w-100 h6 btn py-3 mt-3 btn-primary">확인</button>
+    </div>
   </div>
 </template>
 
