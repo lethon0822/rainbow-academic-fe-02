931b7bd945c1decc2cc2bab6838c1d0d747d829a
mood7dev <mood7dev@gmail.com>
2025-08-27 16:37:06 +0900

    출석부 라디오

--- src/main.js
+++ src/main.js
@@ -8,6 +8,7 @@ import BootstrapVue3 from "bootstrap-vue-3";
 
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue-3/dist/bootstrap-vue-3.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
 
 const pinia = createPinia();
 pinia.use(piniaPluginPersistedstate);
